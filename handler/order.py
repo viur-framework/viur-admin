f01879c32f307d775e051faadaca3c9a938af7e8
Tobias Steinrücken <ts@mausbrand.de>
2013-01-09 16:35:28 +0100

    Fixed the markSend/markPayed actions and added a markCanceled action.

--- handler/order.py
+++ handler/order.py
@@ -4,6 +4,14 @@ from event import event
 from config import conf
 from utils import QueryAggregator
 
+def askYesNo( question ):
+	return( QtGui.QMessageBox.question(	None,
+									QtCore.QCoreApplication.translate("OrderHandler", "Please confirm"),
+									question,
+									QtGui.QMessageBox.Yes | QtGui.QMessageBox.No,
+									QtGui.QMessageBox.No 
+								)==QtGui.QMessageBox.Yes )
+
 class ShopMarkPayedAction( QtGui.QAction ):
 	def __init__(self, parent, *args, **kwargs ):
 		parent = parent
@@ -21,19 +29,27 @@ class ShopMarkPayedAction( QtGui.QAction ):
 			return
 		self.parent().overlay.inform( self.parent().overlay.BUSY )
 		self.request = QueryAggregator()
+		changedOne = False
 		for item in items:
-			#order = service.call( "/order/view", {"id":  item["id"] }, secure=True ) #Fixme: We need a solution for this...
-			#if "is_payed" in  order["values"].keys() and str( order["values"]["is_payed"])=="1":
-			#	if not askYesNo( "Bestellung von %s %s vom %s bereits als Bezahlt markiert. Rechnung erneut zusenden?" % (order["values"]["bill_firstname"], order["values"]["bill_lastname"], order["values"]["creationdate"] ) ):
-			#		continue
+			if "state_payed" in item.keys() and str( item["state_payed"])=="1":
+				if not askYesNo( 
+						QtCore.QCoreApplication.translate(	"OrderHandler",
+														"The order %s %s from %s is allready marked as payed. Resend its bill?" % (	item["bill_firstname"],
+																															item["bill_lastname"],
+																															item["creationdate"] ) ) ):
+					continue
+			changedOne = True
 			self.request.addQuery( NetworkService.request( "/%s/markPayed" % self.parent().modul, {"id":  item["id"] }, secure=True ) )
-		self.connect( self.request, QtCore.SIGNAL("finished(PyQt_PyObject)"), self.onFinished ) 
+		if changedOne:
+			self.connect( self.request, QtCore.SIGNAL("finished(PyQt_PyObject)"), self.onFinished ) 
+		else:
+			self.onFinished( None )
 	
 	def onFinished(self, req ):
 		if self.request.isIdle():
 			self.request.deleteLater()
 			self.request = None
-		self.parent().overlay.inform( self.parent().overlay.SUCCESS )
+		self.parent().reloadData()
 	
 		
 class ShopMarkSendAction( QtGui.QAction ):
@@ -53,19 +69,66 @@ class ShopMarkSendAction( QtGui.QAction ):
 			return
 		self.parent().overlay.inform( self.parent().overlay.BUSY )
 		self.request = QueryAggregator()
+		changedOne = False
 		for item in items:
-			#order = service.call( "/order/view", {"id":  item["id"] }, secure=True ) #Fixme: We need a solution for this...
-			#if "is_payed" in  order["values"].keys() and str( order["values"]["is_payed"])=="1":
-			#	if not askYesNo( "Bestellung von %s %s vom %s bereits als Bezahlt markiert. Rechnung erneut zusenden?" % (order["values"]["bill_firstname"], order["values"]["bill_lastname"], order["values"]["creationdate"] ) ):
-			#		continue
+			if "state_send" in item.keys() and str( item["state_send"])=="1":
+				if not askYesNo( 
+						QtCore.QCoreApplication.translate(	"OrderHandler",
+														"The order %s %s from %s is allready marked as shipped. Repeat the command?" % (	item["bill_firstname"],
+																																item["bill_lastname"],
+																																item["creationdate"] ) ) ):
+					continue
+			changedOne = True
 			self.request.addQuery( NetworkService.request( "/%s/markSend" % self.parent().modul, {"id":  item["id"] }, secure=True ) )
-		self.connect( self.request, QtCore.SIGNAL("finished(PyQt_PyObject)"), self.onFinished ) 
+		if changedOne:
+			self.connect( self.request, QtCore.SIGNAL("finished(PyQt_PyObject)"), self.onFinished ) 
+		else:
+			self.onFinished( None )
 
 	def onFinished(self, req ):
 		if self.request.isIdle():
 			self.request.deleteLater()
 			self.request = None
-		self.parent().overlay.inform( self.parent().overlay.SUCCESS )
+		self.parent().reloadData()
+		
+class ShopMarkCanceledAction( QtGui.QAction ):
+	def __init__(self, parent, *args, **kwargs ):
+		parent = parent
+		self.request = None
+		super( ShopMarkCanceledAction, self ).__init__( QtGui.QIcon("icons/actions/cancel_small.png"), QtCore.QCoreApplication.translate("OrderHandler","Order canceled"), parent )
+		self.connect( self, QtCore.SIGNAL( "triggered(bool)"), self.onTriggered )
+	
+	def onTriggered( self, e ):
+		itemIndexes = []
+		for row in [ x.row() for x in self.parentWidget().selectionModel.selection().indexes() ]:
+			if not row in itemIndexes:
+				itemIndexes.append( row )
+		items = [ self.parentWidget().model.getData()[ x ] for x in itemIndexes ]
+		if QtGui.QMessageBox.question(self.parentWidget(), QtCore.QCoreApplication.translate("OrderHandler", 'Mark shipped'), QtCore.QCoreApplication.translate("OrderHandler", "Cancel %s orders?" ) % len( items ), QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)==QtGui.QMessageBox.No:
+			return
+		self.parent().overlay.inform( self.parent().overlay.BUSY )
+		self.request = QueryAggregator()
+		changedOne = False
+		for item in items:
+			if "state_canceled" in item.keys() and str( item["state_canceled"])=="1":
+				if not askYesNo( 
+						QtCore.QCoreApplication.translate(	"OrderHandler",
+														"The order %s %s from %s is allready marked as canceled. Repeat the command?" % (	item["bill_firstname"],
+																																item["bill_lastname"],
+																																item["creationdate"] ) ) ):
+					continue
+			changedOne = True
+			self.request.addQuery( NetworkService.request( "/%s/markCanceled" % self.parent().modul, {"id":  item["id"] }, secure=True ) )
+		if changedOne:
+			self.connect( self.request, QtCore.SIGNAL("finished(PyQt_PyObject)"), self.onFinished ) 
+		else:
+			self.onFinished( None )
+
+	def onFinished(self, req ):
+		if self.request.isIdle():
+			self.request.deleteLater()
+			self.request = None
+		self.parent().reloadData()
 
 	
 class OrderHandler( QtCore.QObject ):
@@ -82,5 +145,6 @@ class OrderHandler( QtCore.QObject ):
 			return
 		queue.registerHandler( 10, ShopMarkSendAction )
 		queue.registerHandler( 10, ShopMarkPayedAction )
+		queue.registerHandler( 10, ShopMarkCanceledAction )
 
 _orderHandler = OrderHandler()
