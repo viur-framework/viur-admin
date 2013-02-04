4a9fb9567faa1447daedab164da00b07b65c3f64
Tobias Steinrücken <ts@mausbrand.de>
2013-02-04 13:06:38 +0100

    Added support for grouping modules

--- mainwindow.py
+++ mainwindow.py
@@ -3,7 +3,7 @@ from ui.adminUI import Ui_MainWindow
 from PyQt4 import QtCore, QtGui, QtWebKit
 from event import event
 from config import conf
-import time
+import time, os
 from utils import RegisterQueue, showAbout
 from tasks import TaskViewer
 from analytics import AnalytisWidget
@@ -99,6 +99,11 @@ class EntryHandler( QtGui.QTreeWidgetItem ):
 		"""
 		pass
 	
+class GroupHandler( EntryHandler ):
+	"""
+		Toplevel widget for one modul-group
+	"""
+	pass
 
 
 class WidgetHandler( EntryHandler ):
@@ -299,6 +304,13 @@ class MainWindow( QtGui.QMainWindow ):
 			if item.modul == handler.modul:
 				item.addChild( handler )
 				self.ui.treeWidget.expandItem( item )
+			elif isinstance( item, GroupHandler ):
+				for subIdx in range( 0 , item.childCount() ):
+					child = item.child( subIdx )
+					if child.modul == handler.modul:
+						child.addChild( handler )
+						self.ui.treeWidget.expandItem( item )
+						self.ui.treeWidget.expandItem( child )
 		self.ui.treeWidget.sortItems( 0, QtCore.Qt.AscendingOrder )
 	
 	def removeHandler( self, handler ):
@@ -336,13 +348,38 @@ class MainWindow( QtGui.QMainWindow ):
 		data = conf.serverConfig
 		event.emit( QtCore.SIGNAL('downloadedConfig(PyQt_PyObject)'), data )
 		handlers = []
+		groupHandlers = {}
+		if "configuration" in data.keys() and "modulGroups" in data["configuration"].keys():
+			for group in data["configuration"]["modulGroups"]:
+				if not all( [x in group.keys() for x in ["name", "prefix", "icon"] ] ): #Assert that all required properties are there
+					continue
+				groupHandlers[ group["prefix"] ] = GroupHandler( "tl-handler-%s" % group["prefix"] )
+				groupHandlers[ group["prefix"] ].setText(0, group["name"] )
+				groupHandlers[ group["prefix"] ].setText(0, group["name"] )
+				lastDot = group["icon"].rfind(".")
+				smallIcon = group["icon"][ : lastDot ]+"_small"+group["icon"][ lastDot: ]
+				if os.path.isfile( os.path.join( os.getcwd(), smallIcon ) ):
+					groupHandlers[ group["prefix"] ].setIcon( 0, QtGui.QIcon( smallIcon ) )
+				else:
+					groupHandlers[ group["prefix"] ].setIcon( 0, QtGui.QIcon( group["icon"] ) )
+				self.ui.treeWidget.addTopLevelItem( groupHandlers[ group["prefix"] ] )
 		if not "modules" in conf.portal.keys():
 			conf.portal["modules"] = {}
 		for modul, cfg in data["modules"].items():
 			queue = RegisterQueue()
 			event.emit( QtCore.SIGNAL('requestModulHandler(PyQt_PyObject,PyQt_PyObject)'),queue, modul )
 			handler = queue.getBest()()
-			self.ui.treeWidget.addTopLevelItem( handler )
+			if "name" in cfg.keys() and groupHandlers:
+				parent = None
+				for groupName in groupHandlers.keys():
+					if cfg["name"].startswith( groupName ):
+						parent = groupHandlers[ groupName ]
+				if parent:
+					parent.addChild( handler )
+				else:
+					self.ui.treeWidget.addTopLevelItem( handler )
+			else:
+				self.ui.treeWidget.addTopLevelItem( handler )
 			handlers.append( handler )
 			event.emit( QtCore.SIGNAL('modulHandlerInitialized(PyQt_PyObject)'), modul )
 		self.show()
