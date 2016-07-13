3e490980102f891b619617561d0f832eb913eb83
Stefan Kögl <stefan.koegl@rub.de>
2016-07-13 10:42:58 +0200

    optimized startpages

--- viur_admin/startpages/default.py
+++ viur_admin/startpages/default.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from PyQt5 import QtGui, QtWidgets
 
-"""
-	Displayes the ViUR Logo
-"""
-
 
 class DefaultWidget(QtWidgets.QWidget):
+	"""Displayes the ViUR Logo
+	"""
+
 	def __init__(self):
 		super(DefaultWidget, self).__init__()
 		layout = QtWidgets.QVBoxLayout()
--- viur_admin/startpages/webpage.py
+++ viur_admin/startpages/webpage.py
@@ -1,22 +1,18 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from PyQt5 import QtCore, QtWebKitWidgets
+from PyQt5 import QtCore, QtWebKitWidgets, QtWebKit
 
-from viur_admin.network import NetworkService
 from viur_admin.config import conf
-
-"""
-	Displayes the specified webpage.
-"""
+from viur_admin.network import NetworkService, nam
 
 
 class WebWidget(QtWebKitWidgets.QWebView):
+	"""Displays a pre rendered page received from viur server instance
+	"""
+
 	def __init__(self):
 		super(WebWidget, self).__init__()
-		NetworkService.request(
-			"%s%s" % (NetworkService.url.replace("/admin", ""), conf.serverConfig["configuration"]["startPage"]),
-			secure=True, successHandler=self.setHTML)
-
-	def setHTML(self, req):
-		data = bytes(req.readAll()).decode("UTF-8")
-		self.setHtml(data, QtCore.QUrl(NetworkService.url.replace("/admin", "")))
+		self.settings().setAttribute(QtWebKit.QWebSettings.JavascriptEnabled, True)
+		self.settings().setAttribute(QtWebKit.QWebSettings.JavascriptCanOpenWindows, True)
+		self.page().setNetworkAccessManager(nam)
+		self.setUrl(QtCore.QUrl(
+				"%s%s" % (NetworkService.url.replace("/admin", ""), conf.serverConfig["configuration"]["startPage"])))
