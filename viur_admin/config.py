a81b27b9846d6fd5b5c8417021b1a9872a260f3f
Stefan Kögl <stefan.koegl@rub.de>
2016-12-29 16:50:04 +0100

    first draft of cookie storage

--- viur_admin/config.py
+++ viur_admin/config.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import json
+from datetime import datetime
+
 from viur_admin.log import getLogger
 
 logger = getLogger(__name__)
 import os
 from hashlib import sha512
-from pprint import pprint
-from operator  import attrgetter
 from time import time, sleep
+
+from PyQt5 import QtNetwork
 
 
 class Config(object):
@@ -30,8 +33,10 @@ class Config(object):
 		self.loadConfig()
 
 	def xor(self, data, key=None):
-		"""This applys at least some *VERY BASIC* obscurfaction to saved passwords
-		DO NOT RELY ON THIS TO GUARD ANYTHING"""
+		"""This applies at least some *VERY BASIC* obscuring to saved passwords
+
+		DO NOT RELY ON THIS TO GUARD ANYTHING
+		"""
 		key = bytes([x for x in range(0, 254)])
 		klen = len(key)
 		res = []
@@ -53,7 +58,7 @@ class Config(object):
 
 		self.accounts.sort(key=lambda x: x["name"].lower())
 		for account in self.accounts:
-			if not "key" in account.keys():
+			if "key" not in account.keys():
 				account["key"] = int(time())
 				sleep(1)  # Bad hack to ensure key is unique; runs only once at first start
 		# Load rest of the config
@@ -83,8 +88,11 @@ class Config(object):
 		configFileObject.flush()
 		configFileObject.close()
 
-	def loadPortalConfig(self, url):
-		logger.debug("LOADING PORTAL CONFIG")
+	def loadPortalConfig(self, url, withCookies=False, forceReload=False):
+		from viur_admin import network
+		logger.debug("loading portal config for url=%r with cookies=%r and forcingReload=%r", url, withCookies, forceReload)
+		if self.portal and not forceReload:
+			return
 		self.currentPortalConfigDirectory = os.path.join(self.storagePath, sha512(url.encode("UTF-8")).hexdigest())
 		try:
 			if not os.path.isdir(self.currentPortalConfigDirectory):
@@ -93,15 +101,28 @@ class Config(object):
 			configData = configFileObject.read().decode("UTF-8")
 			cfg = json.loads(configData)
 			self.portal = cfg
+			if withCookies:
+				cookies = list(7)
+				now = datetime.now()
+				for plainCookie in self.portal.get("cookies", list()):
+					logger.debug("cookieRaw: %r", plainCookie)
+					restoredCookie = QtNetwork.QNetworkCookie.parseCookies(bytearray(plainCookie, "ascii"))[0]
+					if restoredCookie.expirationDate() > now:
+						logger.debug("restored cookie accepted: %r", restoredCookie)
+						cookies.append(restoredCookie)
+				network.nam.cookieJar().setAllCookies(cookies)
 		except Exception as err:
 			logger.error("Could not load Portal Config")
 			logger.exception(err)
 			self.portal = {"modules": {}}
 
 	def savePortalConfig(self):
+		from viur_admin import network
 		if not self.currentPortalConfigDirectory:
 			return
 		configFileObject = open(os.path.join(self.currentPortalConfigDirectory, "config.dat"), "w+b")
+		# now saving cookies needed
+		self.portal["cookies"] = [str(cookie.toRawForm(), "ascii") for cookie in network.nam.cookieJar().allCookies()]
 		configData = json.dumps(self.portal).encode("UTF-8")
 		configFileObject.write(configData)
 		configFileObject.flush()
