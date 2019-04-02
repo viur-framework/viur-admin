8aa15eab3b434ee26a39a605eb83dd1902729358
Stefan Kögl <stefan.koegl@rub.de>
2019-04-02 21:06:25 +0200

    cleaned up handler.list code and (re-)added list subviews

--- viur_admin/handler/list.py
+++ viur_admin/handler/list.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from viur_admin.log import getLogger
-
-logger = getLogger(__name__)
-
-
 from PyQt5 import QtCore
 
 from viur_admin.config import conf
 from viur_admin.event import event
 from viur_admin.utils import loadIcon, WidgetHandler
 from viur_admin.widgets.list import ListWidget
-import viur_admin.ui.icons_rc
+
+
+logger = getLogger(__name__)
 
 
 class PredefinedViewHandler(WidgetHandler):  # EntryHandler
@@ -20,16 +18,14 @@ class PredefinedViewHandler(WidgetHandler):  # EntryHandler
 	def __init__(self, modul, viewName, *args, **kwargs):
 		config = conf.serverConfig["modules"][modul]
 		myview = [x for x in config["views"] if x["name"] == viewName][0]
-		widgetFactory = lambda: ListWidget(modul, logger.debug("invoking ListWidget"), myview.get("columns", list()), myview.get("filter", list()))
-		if "icon" in myview.keys():
-			if myview["icon"].lower().startswith("http://") or myview["icon"].lower().startswith("https://"):
-				icon = myview["icon"]
-			else:
-				icon = loadIcon(myview["icon"])
-		else:
-
-			icon = loadIcon(":icons/modules/list.svg")
-		super(PredefinedViewHandler, self).__init__(widgetFactory, descr=myview["name"], icon=icon, vanishOnClose=False, *args, **kwargs)
+		icon = loadIcon(myview["icon"])
+		super(PredefinedViewHandler, self).__init__(
+			lambda: ListWidget(modul, myview.get("columns", list()), myview.get("filter", dict())),
+			descr=myview["name"],
+			icon=icon,
+			vanishOnClose=False,
+			*args,
+			**kwargs)
 		self.viewName = viewName
 
 
@@ -38,26 +34,31 @@ class ListCoreHandler(WidgetHandler):  # EntryHandler
 
 	def __init__(self, modul, *args, **kwargs):
 		# Config parsen
-		config = conf.serverConfig["modules"][modul]
-		# logger.debug("ListCoreHandler.init: %r", config.get("name", "NoListCoreHandlerName"), config.get("columns"))
-		actions = config.get("actions")
+		ListCoreHandlerConfig = conf.serverConfig["modules"][modul]
+		logger.debug("ListCoreHandler configuration: %r", ListCoreHandlerConfig)
+		actions = ListCoreHandlerConfig.get("actions")
 		widgetGen = lambda: ListWidget(
 			modul,
-			fields=config.get("columns", list()),
-			filter=config.get("filter", list()),
+			fields=ListCoreHandlerConfig.get("columns", list()),
+			filter=ListCoreHandlerConfig.get("filter", dict()),
 			actions=actions
 		)
+
 		icon = None
-		if "icon" in config.keys() and config["icon"]:
-			icon = loadIcon(config["icon"])
+		if "icon" in ListCoreHandlerConfig and ListCoreHandlerConfig["icon"]:
+			icon = loadIcon(ListCoreHandlerConfig["icon"])
+
 		super(ListCoreHandler, self).__init__(
-				widgetGen, descr=config["name"],
-				icon=icon,
-				sortIndex=config.get("sortIndex", 0),
-				vanishOnClose=False, *args,
-				**kwargs)
-		if "views" in config.keys():
-			for view in config["views"]:
+			widgetGen,
+			descr=ListCoreHandlerConfig["name"],
+			icon=icon,
+			sortIndex=ListCoreHandlerConfig.get("sortIndex", 0),
+			vanishOnClose=False,
+			*args,
+			**kwargs)
+
+		if "views" in ListCoreHandlerConfig.keys():
+			for view in ListCoreHandlerConfig["views"]:
 				self.addChild(PredefinedViewHandler(modul, view["name"]))
 
 
