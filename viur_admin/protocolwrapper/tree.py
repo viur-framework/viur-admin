1bdb9d2a5a70d3dd37441ab47063f494ee9d0d1c
Tobias Steinrücken <ts@mausbrand.de>
2022-02-22 16:44:20 +0100

    Fixed partially moved files may show up twice, removed unused code

--- viur_admin/protocolwrapper/tree.py
+++ viur_admin/protocolwrapper/tree.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import OrderedDict
-from typing import Union, Sequence, Tuple, List, Dict, Any, Optional
+from typing import Union, Sequence, Tuple, List, Dict, Any, Optional, Set
 from PyQt5.QtCore import QModelIndex
 from PyQt5 import QtCore, QtWidgets
 
@@ -194,7 +194,6 @@ class TreeView(QtCore.QAbstractItemModel):
 			"leafKeys": [],
 			"parentNodeKey" : parentKey,
 			"nodeKey": nodeKey,
-			"pendingDeletes": set()
 		}
 		return self._dataDict[idx]
 		#self.beginInsertRows(self.createIndex(0,0,idx) if idx else QModelIndex(), 0, 30)
@@ -333,7 +332,7 @@ class TreeWrapper(ProtocolWrapper):
 		NetworkService.request("/%s/listRootNodes" % self.module, successHandler=self.onRootNodesAvailable)
 		protocolWrapperInstanceSelector.insert(self.protocolWrapperInstancePriority, self.checkForOurModul, self)
 		self.busy = True
-		self.views: Set[TreeView] = WeakSet()
+		self.views: WeakSet[TreeView] = WeakSet()
 		self.rootNodes = []
 		self.pendingDeletes = set()
 		# Map of pending inserts (uploads / adds that have not yet been synced to the server).
@@ -498,7 +497,7 @@ class TreeWrapper(ProtocolWrapper):
 					dataDict["nodeCursorList"].append(cursor)
 				else:
 					dataDict["leafCursorList"].append(cursor)
-			filteredSkelList = [x for x in data["skellist"] if x["key"] not in dataDict["pendingDeletes"]]
+			filteredSkelList = [x for x in data["skellist"] if x["key"] not in self.pendingDeletes]
 			oldLength = len(dataDict["nodeKeys"]) + len(dataDict["leafKeys"])
 			extLength = len(filteredSkelList)
 			treeView.beforeInsertRows(req.node, oldLength, extLength, req.treeType)
@@ -639,7 +638,8 @@ class TreeWrapper(ProtocolWrapper):
 					nodeData = view.resolveInternalDataForNodeKey(node)
 					nodeData["parentNodeKey"] = destNode
 					view.afterRemoveRows(data["nodeKey"], oldIdx, 1, "node")
-					nodeData["pendingDeletes"].add(data["nodeKey"])
+					# We'll treat then as deleted, as injectPendingInsertsIfNeeded will add them anyway
+					self.pendingDeletes.add(data["nodeKey"])
 			for leaf in leafs:
 				data = view.resolveInternalDataForLeafKey(leaf)
 				if data:  # Not all views may display this nodes/leafs
@@ -647,7 +647,8 @@ class TreeWrapper(ProtocolWrapper):
 					view.beforeRemoveRows(data["nodeKey"], oldIdx, 1, "leaf")
 					data["leafKeys"].remove(leaf)
 					view.afterRemoveRows(data["nodeKey"], oldIdx, 1, "leaf")
-					data["pendingDeletes"].add(leaf)
+					# We'll treat then as deleted, as injectPendingInsertsIfNeeded will add them anyway
+					self.pendingDeletes.add(leaf)
 			# Insert these items into the destination node
 			data = view.resolveInternalDataForNodeKey(destNode)
 			if data:  # This view also has the destination node
@@ -737,7 +738,7 @@ class TreeWrapper(ProtocolWrapper):
 					# Also update our parentNode key
 					nodeData = view.resolveInternalDataForNodeKey(node)
 					view.afterRemoveRows(data["nodeKey"], oldIdx, 1, "node")
-					data["pendingDeletes"].add(node)
+					self.pendingDeletes.add(node)
 					view.removeInternalDataForNodeKey(node)
 			for leaf in leafs:
 				data = view.resolveInternalDataForLeafKey(leaf)
@@ -746,7 +747,7 @@ class TreeWrapper(ProtocolWrapper):
 					view.beforeRemoveRows(data["nodeKey"], oldIdx, 1, "leaf")
 					data["leafKeys"].remove(leaf)
 					view.afterRemoveRows(data["nodeKey"], oldIdx, 1, "leaf")
-					data["pendingDeletes"].add(node)
+					self.pendingDeletes.add(leaf)
 		# Update our pending insert map
 		for node in nodes:
 			print(self._entityCache[node])
@@ -768,321 +769,6 @@ class TreeWrapper(ProtocolWrapper):
 		for node in nodes:
 			self.taskqueue.addTask(DeleteItemsTask(self, "node", node))
 
-
-
-
-	"""
-	def requestNextBatch(self, treeView, treeType):
-		print("requestNextBatch %s" % treeType, treeView)
-		queryDict = copy.deepcopy(treeView.filterDict)
-		if treeType == "node":
-			if treeView._nodeCursorList:
-				queryDict["cursor"] = treeView._nodeCursorList[-1]
-		r = NetworkService.request("/%s/list/%s" % (self.module, treeType), queryDict, successHandler=self.addCacheData, failureHandler=self.fetchFailed)
-		r.treeType = treeType
-		r.treeView = treeView
-
-
-
-
-
-
-
-
-
-
-
-	def checkBusyStatus(self) -> None:
-		busy = False
-		for child in self.children():
-			if isinstance(child, RequestWrapper) or isinstance(child, RequestGroup):
-				if not child.hasFinished:
-					busy = True
-					break
-		if busy != self.busy:
-			self.busy = busy
-			self.busyStateChanged.emit(busy)
-		QtCore.QCoreApplication.processEvents()
-
-	def checkForOurModul(self, moduleName: str) -> bool:
-		return self.module == moduleName
-
-	def clearCache(self) -> None:
-		" ""
-			Resets our Cache.
-			Does not emit entitiesChanged!
-		" ""
-		self.dataCache = {}
-		for node in self.rootNodes:
-			node = node.copy()
-			node["_type"] = "node"
-			node["key"] = node["key"]
-			node["_isRootNode"] = 1
-			node["parententry"] = None
-			self.dataCache[node["key"]] = node
-
-	def onStructureAvailable(self, req: RequestWrapper) -> None:
-		tmp = NetworkService.decode(req)
-		if tmp is None:
-			self.checkBusyStatus()
-			return
-		for stype, structlist in tmp.items():
-			structure: OrderedDict = OrderedDict()
-			for k, v in structlist:
-				structure[k] = v
-			if stype == "viewNodeSkel":
-				self.viewNodeStructure = structure
-			elif stype == "viewLeafSkel":
-				self.viewLeafStructure = structure
-			elif stype == "editNodeSkel":
-				self.editNodeStructure = structure
-			elif stype == "editLeafSkel":
-				self.editLeafStructure = structure
-			elif stype == "addNodeSkel":
-				self.addNodeStructure = structure
-			elif stype == "addLeafSkel":
-				self.addLeafStructure = structure
-			else:
-				raise ValueError("onStructureAvailable: unknown node type: {0}".format(stype))
-		self.onModulStructureAvailable.emit()
-		self.checkBusyStatus()
-
-	def onRootNodesAvailable(self, req: RequestWrapper) -> None:
-		tmp = NetworkService.decode(req)
-		if isinstance(tmp, list):
-			self.rootNodes = tmp
-		self.clearCache()
-		self.rootNodesAvailable.emit()
-		self.checkBusyStatus()
-		logger.debug("TreeWrapper.onRootNodesAvailable: %r", tmp)
-
-	def childrenForNode(self, node: str) -> str:
-		assert isinstance(node, str)
-		res = []
-		for item in self.dataCache.values():
-			if isinstance(item, dict):  # It's a "normal" item, not a customQuery result
-				if item["parententry"] == node:
-					res.append(item)
-		return res
-
-	def cacheKeyFromFilter(self, node: str, filters: dict) -> str:
-		tmp = {k: v for k, v in filters.items()}
-		tmp["node"] = node
-		tmpList = list(tmp.items())
-		tmpList.sort(key=lambda x: x[0])
-		return "&".join(["%s=%s" % (k, v) for (k, v) in tmpList])
-
-	def queryData(self, node: str, **kwargs: Any) -> str:
-		logger.debug("TreeWrapper.queryData: %r, %r", node, kwargs)
-		print("start query data", node, kwargs)
-		key = self.cacheKeyFromFilter(node, kwargs)
-		if key in self.dataCache:
-			if self.dataCache[key] is None:
-				# We already started fetching that key
-				return key
-			self.deferredTaskQueue.append(("entitiesChanged", key))
-			QtCore.QTimer.singleShot(25, self.execDefered)
-			return key
-
-		# It's a cache-miss or cache too old
-		self.dataCache[key] = None
-		queryParams = {k: v for k, v in kwargs.items()}
-		if "search" in kwargs:
-			queryParams["parentrepo"] = node
-		else:
-			queryParams["parententry"] = node
-		for skelType in ["node", "leaf"]:
-			queryParams["skelType"] = skelType
-			queryParams["amount"] = self.batchSize
-			r = NetworkService.request("/%s/list" % self.module, queryParams, successHandler=self.addCacheData)
-			r.wrapperCacheKey = key
-			r.skelType = skelType
-			r.node = node
-			r.queryArgs = kwargs
-		if self.rootNodes is None or node not in [x["key"] for x in self.rootNodes]:  # Don't query rootNodes again..
-			r = NetworkService.request("/%s/view/node/%s" % (self.module, node), successHandler=self.addCacheData)
-			r.wrapperCacheKey = node
-			r.skelType = "node"
-			r.node = node
-			r.queryArgs = kwargs
-		self.checkBusyStatus()
-		return key
-
-	def queryEntry(self, key: str, skelType: str) -> str:
-		logger.debug("TreeWrapper.queryEntry: %r, %r", key, skelType)
-		if key in self.dataCache:
-			self.deferredTaskQueue.append(("entityAvailable", key))
-			QtCore.QTimer.singleShot(25, self.execDefered)
-			return key
-		r = NetworkService.request("/%s/view/%s/%s" % (self.module, skelType, key), successHandler=self.addCacheData)
-		r.wrapperCacheKey = key
-		r.queryArgs = None
-		r.skelType = skelType
-		r.node = key
-		return key
-
-	def execDefered(self, *args: Any, **kwargs: Any) -> None:
-		logger.debug("TreeWrapper.execDefered: %r, %r", args, kwargs)
-		m, key = self.deferredTaskQueue.pop(0)
-		if m == "entitiesChanged":
-			self.entitiesChanged.emit(key)
-		elif m == "entityAvailable":
-			self.entityAvailable.emit(self.dataCache[key])
-		self.checkBusyStatus()
-
-	def getNode(self, node: str) -> Union[str, None]:
-		logger.debug("TreeWrapper.getNode: %r", node)
-		if node in self.dataCache:
-			return self.dataCache[node]
-		return None
-
-	def getNodesForCustomQuery(self, key: str) -> Union[str, None]:
-		if key not in self.dataCache or self.dataCache[key] is None:
-			return None
-		else:
-			return self.dataCache[key]
-
-
-
-	def add(self, node: str, skelType: str, **kwargs: Any) -> str:
-		tmp = kwargs.copy()
-		tmp["node"] = node
-		tmp["skelType"] = skelType
-		req = NetworkService.request(
-			"/%s/add/" % self.module, tmp, secure=(len(kwargs) > 0),
-			finishedHandler=self.onSaveResult)
-		if not kwargs:
-			# This is our first request to fetch the data, dont show a missing hint
-			req.wasInitial = True
-		else:
-			req.wasInitial = False
-		self.checkBusyStatus()
-		return str(id(req))
-
-	def edit(self, key: str, skelType: str, **kwargs: Any) -> str:
-		req = NetworkService.request(
-			"/%s/edit/%s/%s" % (self.module, skelType, key), kwargs, secure=(len(kwargs) > 0),
-			finishedHandler=self.onSaveResult)
-		if not kwargs:
-			# This is our first request to fetch the data, don't show a missing hint
-			req.wasInitial = True
-		else:
-			req.wasInitial = False
-		self.checkBusyStatus()
-		return str(id(req))
-
-	def editPreflight(self, key: str, node: str, skelType: str, data, callback) -> None:
-		data["bounce"] = "1"
-		data["skey"] = "-"
-		url = "/%s/edit/%s/%s" % (self.module, skelType, key) if key else "/%s/add/%s/%s" % (self.module, skelType, node)
-		req = NetworkService.request(url, data, finishedHandler=callback)
-
-	def deleteEntities(self, nodes: list, leafs: list) -> None:
-		"" "Delete files and/or directories from the server.
-
-		Nodes will be deleted recursively
-
-		:param nodes: Nodes to be removed
-		:type nodes: list
-		:param leafs: leafs to be removed
-		:type leafs: list
-		:return:
-		"" "
-		request = RequestGroup(finishedHandler=self.delayEmitEntriesChanged)
-		self._requestGroups.append(request)
-		for leaf in leafs:
-			request.addRequest("/{0}/delete/leaf/{1}".format(self.module, leaf), secure=True)
-		for node in nodes:
-			request.addRequest("/{0}/delete/node/{1}".format(self.module, node), secure=True)
-		# request.flushList = [ lambda *args, **kwargs:  self.flushCache( rootNode, path ) ]
-		request.queryType = "delete"
-		self.checkBusyStatus()
-		return str(id(request))
-
-	def move(self, nodes: Sequence[str], leafs: Sequence[str], destNode: str) -> str:
-		"" "Moves elements to the given rootNode/path.
-
-		:param nodes: Nodes to be removed
-		:type nodes: list
-		:param leafs: leafs to be removed
-		:type leafs: list
-		:param destNode: destination node id
-		:type destNode: str
-		"" "
-		request = RequestGroup(finishedHandler=self.delayEmitEntriesChanged)
-		self._requestGroups.append(request)
-		for node in nodes:
-			request.addQuery(NetworkService.request(
-				"/%s/move" % self.module,
-				{
-					"key": node,
-					"skelType": "node",
-					"parentNode": destNode
-				},
-				parent=self, secure=True))
-		for leaf in leafs:
-			request.addQuery(NetworkService.request(
-				"/%s/move" % self.module,
-				{
-					"key": leaf,
-					"skelType": "leaf",
-					"parentNode": destNode
-				},
-				parent=self, secure=True))
-		request.queryType = "move"
-		self.checkBusyStatus()
-		return str(id(request))
-
-	def delayEmitEntriesChanged(self, req: RequestWrapper = None, *args: Any, **kwargs: Any) -> None:
-		"" "Give GAE a chance to apply recent changes and then force all open views of that module to reload its data
-
-		:param req:
-		:param args:
-		:param kwargs:
-		:return:
-		"" "
-		logger.debug("TreeWrapper.deferredTaskQueue: %r", req)
-		if req is not None:
-			try:
-				logger.debug(NetworkService.decode(req))
-			except:
-				pass
-			try:
-				self._requestGroups.remove(req)
-				logger.debug("request group removed")
-			except Exception as err:
-				logger.exception(err)
-				logger.error("request group could not be removed")
-				pass
-		QtCore.QTimer.singleShot(self.updateDelay, self.emitEntriesChanged)
-
-	def onSaveResult(self, req: RequestWrapper = None) -> None:
-		try:
-			data = NetworkService.decode(req)
-		except:  # Something went wrong, call ErrorHandler
-			self.updatingFailedError.emit(str(id(req)))
-			QtCore.QTimer.singleShot(self.updateDelay, self.resetOnError)
-			return
-		if data["action"] in ["addSuccess", "editSuccess", "deleteSuccess"]:  # Saving succeeded
-			QtCore.QTimer.singleShot(self.updateDelay, self.emitEntriesChanged)
-			self.updatingSucceeded.emit(str(id(req)))
-		else:  # There were missing fields
-			self.updatingDataAvailable.emit(str(id(req)), data, req.wasInitial)
-		self.checkBusyStatus()
-
-	def emitEntriesChanged(self, *args: Any, **kwargs: Any) -> None:
-		logger.debug("TreeWrapper.emitEntriesChanged: %r, %r", args, kwargs)
-		self.clearCache()
-		self.entitiesChanged.emit("")
-		self.checkBusyStatus()
-
-	def resetOnError(self, *args: Any, **kwargs: Any) -> None:
-		"" "
-			If one or more requests fail, flush our cache and force
-			all listening widgets to reload.
-		"" "
-		self.emitEntriesChanged()
-	"""
 
 
 def CheckForTreeModul(moduleName: str, moduleList: dict) -> bool:
